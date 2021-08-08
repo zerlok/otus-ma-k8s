98c2c26315f0b5d812f53ade40a317899e96f7fd
zerlok <denergytro@gmail.com>
2021-08-09 00:19:47 +0300

    WIP: fastapi server created

--- src/echo/__main__.py
+++ src/echo/__main__.py
@@ -1,34 +1,47 @@
+import json
 import logging
 import os
 import socket
 import sys
 import typing as t
 
-from aiohttp import web
+import uvicorn
+from fastapi import APIRouter, FastAPI, Request
+from pydantic import BaseModel, HttpUrl
+
+router = APIRouter()
 
 
-async def handle_echo_request(request: web.Request) -> web.Response:
-    return web.json_response(
-        data={
-            "hostname": socket.gethostname(),
-            "method": request.method,
-            "path": request.path,
-            "headers": dict(request.headers),
-            "body": (await request.json()) if request.body_exists else None,
-        },
-    )
+class DebugResponse(BaseModel):
+    hostname: str
+    method: str
+    url: HttpUrl
+    headers: t.Mapping[str, str]
+    # FIXME: swagger displays type `string`
+    body: t.Any
 
 
-async def handle_health_check(request: web.Request) -> web.Response:
-    return web.json_response(
-        data={
-            "status": "OK",
-        },
-    )
+class HealthCheckResponse(BaseModel):
+    status: str
 
 
-def print_nothing(*args: t.Any, **kwargs: t.Any) -> None:
-    pass
+if os.getenv("DEBUG", "false").lower() in {"1", "yes", "true"}:
+    # FIXME: handler may receive one optional parameter
+    @router.api_route("/", response_model=DebugResponse)
+    async def handle_debug_request(request: Request) -> DebugResponse:
+        body = await request.body()
+        return DebugResponse(
+            hostname=socket.gethostname(),
+            method=request.method,
+            url=str(request.url),
+            headers=dict(request.headers),
+            body=json.loads(body) if body else None,
+        )
+
+
+@router.get("/health")
+async def handle_health_check() -> HealthCheckResponse:
+    return HealthCheckResponse(status="OK")
 
 
 def main(args: t.Sequence[str]) -> int:
@@ -36,20 +49,11 @@ def main(args: t.Sequence[str]) -> int:
         level=logging.DEBUG,
     )
 
-    app = web.Application(
-        middlewares=(
-            web.normalize_path_middleware(),
-        ),
-    )
+    app = FastAPI()
+    app.include_router(router)
 
-    app.router.add_route("*", "/", handle_echo_request)
-    app.router.add_route("GET", "/health", handle_health_check)
-
-    web.run_app(
-        app=app,
-        port=int(os.getenv("HTTP_PORT", 8000)),
-        print=print_nothing,
-    )
+    # noinspection PyTypeChecker
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("HTTP_PORT", 8000)))
 
     return 0
 
